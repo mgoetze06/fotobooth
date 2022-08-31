e699714596f971fa87bf96cbb9864a6b38dd30f9
Maurice <99mg24@web.de>
2022-08-31 17:00:05 +0200

    added animation between two images (slide new images from right to left)

--- cvml/cv2_ShowTwoImagesAfterEachOther.py
+++ cvml/cv2_ShowTwoImagesAfterEachOther.py
@@ -5,6 +5,37 @@ import time # not really needed, used to simulate the 2 seconds of generation
 
 lock = Lock()
 
+
+def slideBetweenImages(old, new, i,totalSteps):
+    # slide between images from right to left (new image comes from rigth)
+
+    w, h = 1920, 1080
+    anImg = np.zeros((h,w,3), np.uint8)
+    #anImg = np.array([[]])
+    #print(anImg.shape)
+    #print(old.shape)
+    #totalSteps = 50
+    pixelShift = int(w / totalSteps+1)
+    #transitionDone = False
+
+    #for i in range(steps):
+    if i == totalSteps:
+        return new
+    else:
+        print(i)
+        print(pixelShift)
+        print(pixelShift * i)
+        temp = pixelShift * i
+        if temp > w:
+            temp = w
+        anImg[0:h,0:(w - temp)] = old[0:h,(temp):w]
+        anImg[0:h,(w - temp):w] = new[0:h,0:temp]
+    #with lock:
+    #    imGen.image = anImg
+    #time.sleep()
+    #transitionDone = True
+    return anImg
+
 class ImageGenerator:
     def __init__(self, src=0):
       # initialize it with zeros to always have something to show. You can set it to None and check it outside before displaying
@@ -23,23 +54,41 @@ class ImageGenerator:
 
     def update(self):
       # go until stop is called, you can set other criterias
+      i = 0
+      totalSteps = 24
+      old = cv2.imread("collage.jpg")
+      new = cv2.imread("selection_collage.jpg")
       while True:
         if self.stopped:
           return
-        # generate the image, this is equal to finalimage = generate_image() in your code
+        showImg = False
 
-        image = np.random.randint(0,255, (600, 800, 3), dtype=np.uint8)
+        if showImg:
+            # generate the image, this is equal to finalimage = generate_image() in your code
 
-        if self.first:
-            image = cv2.imread("collage.jpg")
-            self.first = False
+            image = np.random.randint(0,255, (600, 800, 3), dtype=np.uint8)
+
+            if self.first:
+                image = cv2.imread("collage.jpg")
+                self.first = False
+            else:
+                image = cv2.imread("selection_collage.jpg")
+                self.first = True
+            # this sleep is to simulate that it took longer to execute
+            time.sleep(2)
+            with lock:
+              self.frame = image
         else:
-            image = cv2.imread("selection_collage.jpg")
-            self.first = True
-        # this sleep is to simulate that it took longer to execute
-        time.sleep(2)
-        with lock:
-          self.frame = image
+
+            image = slideBetweenImages(old, new, i,totalSteps)
+            if i <= totalSteps:
+                i += 1
+                with lock:
+                    self.frame = image
+            else:
+                with lock:
+                    self.frame = new
+            time.sleep(0.001)
 
     # if this changes the other thread will stop
     def stop(self):
