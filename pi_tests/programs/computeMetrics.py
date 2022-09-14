ce626610ff18fbef7c281dac0c975fc70bc85035
Maurice <99mg24@web.de>
2022-09-14 16:56:06 +0200

    pi metrics test on test dataset (140samples of binary classes thumbs up and thumbs down)

--- pi_tests/programs/computeMetrics.py
+++ pi_tests/programs/computeMetrics.py
@@ -5,10 +5,9 @@ from sklearn import metrics
 if not os.path.exists('test.npy'):
     print("no array found")
     exit
-with open('test.npy', 'rb') as f:
+with open('C:/Users/Maurice/Desktop/test.npy', 'rb') as f:
     a = np.load(f)
 print(a)
-print("true labels:")
 labels = []
 pred = []
 prob = []
@@ -16,27 +15,31 @@ file = []
 time = []
 for row in a:
     if not row[0] == "detectiontime [s]":
-        time.append(row[0])
+        time.append(float(row[0]))
         file.append(row[1])
         pred.append(row[3])
         prob.append(row[4])
         labels.append(row[5])
 #labels = a[:][0]
-print(labels)
-print(pred)
-print(prob)
-print(file)
-print(time)
+#print(labels)
+#print(pred)
+#print(prob)
+#print(file)
+#print(time)
 
 #conditions = [(labels =='okay'),
 #              (labels =='peac'),
 #              (labels =='RECOVERING')]
 #choices = [int(1), int(0), int(2)]
 #data['operation'] = np.select(conditions, choices, default=0)
-
+print("unique predictions:")
 unique, counts = np.unique(pred, return_counts=True)
 print(dict(zip(unique, counts)))
-
+print("unique true labels:")
+unique, counts = np.unique(labels, return_counts=True)
+print(dict(zip(unique, counts)))
 print("Accuracy:",metrics.accuracy_score(labels, pred))
 print(metrics.confusion_matrix(labels, pred))
 print(metrics.classification_report(labels, pred))
+
+print(np.mean(time))
