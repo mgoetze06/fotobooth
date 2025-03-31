e8a91145f41adf24e513c970a4efd8f43185f26c
Maurice Götze <99mg24@web.de>
2025-03-31 21:52:34 +0200

    place in widget using tk

--- liveview/usbwebcam.py
+++ liveview/usbwebcam.py
@@ -2,25 +2,25 @@ import cv2
 import tkinter as tk
 from PIL import Image, ImageTk
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import tkinter as tk
 from PIL import Image, ImageTk
 import multiprocessing
 import time
 import os, random, shutil
-from luma.core.interface.serial import i2c
-from luma.core.render import canvas
-from luma.oled.device import sh1106
-import _rpi_ws281x as ws
-import subprocess
-from subprocess import check_output
-import psutil
-from gpiozero import CPUTemperature
+#from luma.core.interface.serial import i2c
+#from luma.core.render import canvas
+#from luma.oled.device import sh1106
+#import _rpi_ws281x as ws
+#import subprocess
+#from subprocess import check_output
+#import psutil
+#from gpiozero import CPUTemperature
 from datetime import datetime
 import cv2
 import io
-import gphoto2 as gp
-from webserver.fotobooth_utils import writeImagecountToFile,writeCollageCountToFile,readRGBFromFile,IsCustomCollageEnabled
+#import gphoto2 as gp
+#from webserver.fotobooth_utils import writeImagecountToFile,writeCollageCountToFile,readRGBFromFile,IsCustomCollageEnabled
 
 
 def resizeImageToCanvas(pilImage,w,h):
@@ -29,10 +29,10 @@ def resizeImageToCanvas(pilImage,w,h):
         ratio = min(w/imgWidth, h/imgHeight)
         imgWidth = int(imgWidth*ratio)
         imgHeight = int(imgHeight*ratio)
-        pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+        pilImage = pilImage.resize((imgWidth,imgHeight))
 
         try:
-            r,g,b = readRGBFromFile()
+            r,g,b = 0,0,0#readRGBFromFile()
             new_img= Image.new(mode="RGB", size=(scr_w,scr_h), color=(r,g,b))
             new_img.paste(pilImage, (round(scr_w/2-imgWidth/2),0))
             pilImage = new_img
@@ -75,33 +75,33 @@ if __name__ == '__main__':
     scr_w = 1920
     scr_h = 1080
     try:
-        pilImage = Image.open('/home/pi/programs/countdown/picwait.jpg')
+        pilImage = Image.open('picwait.jpg')
     except:
         pass
 
     pilImage = resizeImageToCanvas(pilImage,w,h)
 
-    #updateCanvas(pilImage,root,canvas)
     image = ImageTk.PhotoImage(pilImage)
     imagesprite = canvas.create_image(w/2,h/2,image=image)
     root.update()
     newimage = False
-    cap = cv2.VideoCapture(cv2.CAP_V4L2)
+    livePreview = True
 
-    livecanvas = tk.Canvas(canvas, width=cap.get(cv2.CAP_PROP_FRAME_WIDTH), 
-                                height=cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    livecanvas.pack()
-    i = 0
-    while i < 100:
-        ret, frame = cap.read()
-        
-        if ret:
-            # Convert image from OpenCV BGR format to RGB
-            photo = convert_to_tkimage(frame)
+    if livePreview:
+        print("accessing webcam stream")
+        cap = cv2.VideoCapture(cv2.CAP_V4L2)
+        livecanvas = tk.Canvas(root, width=cap.get(cv2.CAP_PROP_FRAME_WIDTH),height=cap.get(cv2.CAP_PROP_FRAME_HEIGHT),highlightthickness=0)
+        livecanvas.place(in_= canvas, x = 0, y = 0)
+        i = 0
+        while i < 100:
+            ret, frame = cap.read()
             
-            # Update the canvas with the new image
-            livecanvas.create_image(0, 0, image=photo, anchor=tk.NW)
-            print("updated image")
-
-        root.update()
-        i += 1
+            if ret:
+                photo = convert_to_tkimage(frame)
+                
+                livecanvas.create_image(0, 0, image=photo, anchor=tk.NW)
+                print("updated image")
+            else:
+                print("error accessing camera")
+            root.update()
+            i += 1
